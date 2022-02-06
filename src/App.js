7409964b1c8e045ea8edea786c5ec29315c013f6
AteevDuggal <akd3257@gmail.com>
2022-02-06 15:22:18 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 // import Form from "./Forms/Form";
-// import LandingPage from "./Landing Page/LandingPage";
-import About_Main from "./About_Main"
+import LandingPage from "./Landing Page/LandingPage";
+// import About_Main from "./About_Main"
 const App = () => {
   return (
     <>
       {/* <Form /> */}
-      {/* <LandingPage /> */}
-      <About_Main />
+      <LandingPage />
+      {/* <About_Main /> */}
     </>
   );
 };
